cfea809bd9a185baa6437cb7b0da324e673eeedc
i023092 <amadou.mamane-sarki@rcibanque.com>
2022-03-15 14:07:03 +0100

    Added documentation

---
+++ src/main/scala/com/mydevs/database/bigquery/package.scala
@@ -0,0 +1,13 @@
+package com.mydevs.database
+
+/**
+ * Contains utilities for interacting with Big Query using Spark
+ * This includes :
+ *  - writing spark dataframes into Big Query
+ *  - Loading Big Query tables into spark
+ *  - Creating Big Query tables dynamically
+ *  - Handling Big Decimal values
+ */
+package object bigquery {
+
+}
